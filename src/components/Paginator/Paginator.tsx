bfba4c8daf8926e2fc47f78871c23d71ad4273e9
ChaoChaoChao <chao.li@usask.ca>
2023-10-15 02:43:12 +0800

    Update Paginator.tsx
    remove Paginator to bottom.

--- src/components/Paginator/Paginator.tsx
+++ src/components/Paginator/Paginator.tsx
@@ -13,87 +13,59 @@ const Paginator = ({
   let leftCount = 0;
   let rightCount = 0;
 
+  const baseStyle = {
+    marginX: "2",
+    bg: "gray.200",
+    _hover: {
+      bg: "gray.300",
+    },
+  };
+
+  const activeStyle = {
+    ...baseStyle,
+    bg: "cyan.400",
+    color: "white",
+    _hover: {
+      bg: "cyan.300",
+    },
+  };
+
   if (pages.length <= 5) {
     return (
-      <Flex>
+      <Flex justify="center">
+        <Button {...baseStyle} onClick={() => setIndex(0)}>{`<<`}</Button>
+        <Button {...baseStyle} onClick={() => setIndex(0)}>{`<`}</Button>
+        {pages.map((p) => (
+          <Button
+            {...(index === p ? activeStyle : baseStyle)}
+            onClick={() => setIndex(p)}
+          >
+            {p + 1}
+          </Button>
+        ))}
         <Button
-          onClick={() => {
-            setIndex(0);
-          }}
-        >{`<<`}</Button>
-        <Button
-          onClick={() => {
-            setIndex(0);
-          }}
-        >{`<`}</Button>
-        {pages.map((p) => {
-          return index === p ? (
-            <Button
-              colorScheme="cyan"
-              onClick={() => {
-                setIndex(p);
-              }}
-            >
-              {p + 1}
-            </Button>
-          ) : (
-            <Button
-              onClick={() => {
-                setIndex(p);
-              }}
-            >
-              {p + 1}
-            </Button>
-          );
-        })}
-        <Button
-          onClick={() => {
-            setIndex(index === pages.length - 1 ? pages.length - 1 : index + 1);
-          }}
+          {...baseStyle}
+          onClick={() => setIndex(index === pages.length - 1 ? pages.length - 1 : index + 1)}
         >{`>`}</Button>
-        <Button
-          onClick={() => {
-            setIndex(pages.length - 1);
-          }}
-        >{`>>`}</Button>
+        <Button {...baseStyle} onClick={() => setIndex(pages.length - 1)}>{`>>`}</Button>
       </Flex>
     );
   } else {
     return (
-      <Flex>
+      <Flex justify="center">
+        <Button {...baseStyle} onClick={() => setIndex(0)}>{`<<`}</Button>
+        <Button {...baseStyle} onClick={() => setIndex(index > 0 ? index - 1 : 0)}>{`<`}</Button>
         <Button
-          onClick={() => {
-            setIndex(0);
-          }}
-        >{`<<`}</Button>
-        <Button
-          onClick={() => {
-            setIndex(index > 0 ? index - 1 : 0);
-          }}
-        >{`<`}</Button>
-        <Button
-          colorScheme={index === 0 ? "cyan" : "gray"}
-          onClick={() => {
-            setIndex(0);
-          }}
+          {...(index === 0 ? activeStyle : baseStyle)}
+          onClick={() => setIndex(0)}
         >{`1`}</Button>
         {pages.slice(1, pages.length - 1).map((p, i) => {
           const j = i + 1;
           if (index === j) {
-            return index === p ? (
+            return (
               <Button
-                colorScheme="cyan"
-                onClick={() => {
-                  setIndex(p);
-                }}
-              >
-                {p + 1}
-              </Button>
-            ) : (
-              <Button
-                onClick={() => {
-                  setIndex(p);
-                }}
+                {...(index === p ? activeStyle : baseStyle)}
+                onClick={() => setIndex(p)}
               >
                 {p + 1}
               </Button>
@@ -101,31 +73,24 @@ const Paginator = ({
           } else {
             if (j < index && !leftCount) {
               leftCount++;
-              return <Button isDisabled>...</Button>;
+              return <Button {...baseStyle} isDisabled>...</Button>;
             } else if (j > index && !rightCount) {
               rightCount++;
-              return <Button isDisabled>...</Button>;
+              return <Button {...baseStyle} isDisabled>...</Button>;
             }
           }
         })}
         <Button
-          colorScheme={index === pages.length - 1 ? "cyan" : "gray"}
-          onClick={() => {
-            setIndex(pages.length - 1);
-          }}
+          {...(index === pages.length - 1 ? activeStyle : baseStyle)}
+          onClick={() => setIndex(pages.length - 1)}
         >
           {pages.length}
         </Button>
         <Button
-          onClick={() => {
-            setIndex(index === pages.length - 1 ? pages.length - 1 : index + 1);
-          }}
+          {...baseStyle}
+          onClick={() => setIndex(index === pages.length - 1 ? pages.length - 1 : index + 1)}
         >{`>`}</Button>
-        <Button
-          onClick={() => {
-            setIndex(pages.length - 1);
-          }}
-        >{`>>`}</Button>
+        <Button {...baseStyle} onClick={() => setIndex(pages.length - 1)}>{`>>`}</Button>
       </Flex>
     );
   }
