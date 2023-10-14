5185e48655663c5eca1d5042d068a191fccdb72f
ChaoChaoChao <chao.li@usask.ca>
2023-10-15 02:42:09 +0800

    Update NewsUpdates.tsx
    fix(navigation): Corrected issue preventing navigation to detail page

--- src/routes/NewsUpdates/NewsUpdates.tsx
+++ src/routes/NewsUpdates/NewsUpdates.tsx
@@ -18,7 +18,7 @@ function NewsUpdates() {
   const [news, setNews] = useState<OneDriveFile[]>([]);
   const [index, setIndex] = useState<number>(0);
   const [pages, setPages] = useState<number[]>([]);
-  const pageSizes = 5;
+  const pageSizes = 7;
 
   const sharingLink = `https://1drv.ms/f/s!Akwk_rooMFtbgRZsFanDS-fleX-m?e=1jz0gf`;
   const driveAdapter = new OneDriveAdapter(sharingLink);
@@ -40,19 +40,21 @@ function NewsUpdates() {
           const a = Array.from(Array(1).keys());
           for (const _ of a) {
             mdList.push(child as OneDriveFile);
+            // console.log(child)
           }
         }
       }
 
-      //TODO should not be any
       let updatedNews = mdList.map((item: any) => {
-        const nameParts = item.name.split('-'); 
-        const date = nameParts.slice(0, 3).join('-'); // get date
-        const title = nameParts.slice(3).join('-'); // get title
+        const nameParts = item.name.split('-');
+        const date = nameParts.slice(0, 3).join('-');
+        const title = nameParts.slice(3).join('-');
+        const contentAsText = item.contentAsText
         return {
-          ...item,
-          name: title.replace('.md', ''), // remove .md
-          date: date, // 存储日期
+          name: title.replace('.md', ''),
+          date: date,
+          contentAsText: contentAsText,
+          ...item
         };
       });
 
@@ -64,46 +66,53 @@ function NewsUpdates() {
     fetchData();
   }, []);
 
+
+  console.log(news)
+
   const redirectToDetail = async (contextLink: Promise<string | undefined>) => {
+    // console.log(n.contentAsText)
     history.pushState({ context: await contextLink }, "", "/news/detail");
     history.go();
   };
 
   return (
-    <Box bg={useColorModeValue("gray.50", "gray.900")} color={useColorModeValue("gray.800", "white")} w="100%" minHeight="90vh" p={4}>
+    <Box bg={useColorModeValue("gray.50", "gray.900")} color={useColorModeValue("gray.800", "white")} w="100%" minH="90vh" p={4}>
       <Box maxW="1200px" margin="0 auto" p={5} pt="50px">
         <Heading mb={6} marginBottom="6rem" textAlign="center" as="h1" size="2xl" fontWeight="bold" color="blue.500">
           新闻更新
         </Heading>
 
-        {news ? (
-          <Accordion allowToggle w="100%">
-            {news.slice(index * pageSizes, (index + 1) * pageSizes).map((n:OneDriveFile | any, i) => (
-              <AccordionItem
-                bg={cardBg}
-                border="1px"
-                borderColor={cardBorder}
-                borderRadius="md"
-                transition={hoverTransition}
-                _hover={{ transform: hoverScale, boxShadow: hoverShadow }}
-                marginBottom={i === news.length - 1 ? "0" : "1.5rem"}
-              >
-                <AccordionButton onClick={() => redirectToDetail(n.contentAsText)}>
-                  <Box flex="1" textAlign="left" fontSize="1.25rem" fontWeight="semibold" color={cardTextColor}>
-                    <Icon as={FiFileText} marginRight="12px" />
-                    {n.name} ｜ {n.date}
-                  </Box>
-                </AccordionButton>
-                <Text px={12} pb={4} color="gray.600">
-                  点击上方,开始阅读{n.name}的相关文章。
-                </Text>
-              </AccordionItem>
-            ))}
-            <Paginator index={index} setIndex={setIndex} pages={pages} />
-          </Accordion>
-        ) : (
-          <CircularProgress isIndeterminate color="blue.600" />
-        )}
+        <Box display="flex" flexDirection="column" minHeight="calc(80vh - 50px - 6rem)">
+          {news.length ? (
+            <Accordion allowToggle w="100%" flex="1">
+              {news.slice(index * pageSizes, (index + 1) * pageSizes).map((n: OneDriveFile | any, i) => (
+                <AccordionItem
+                  key={i}
+                  bg={cardBg}
+                  border="1px"
+                  borderColor={cardBorder}
+                  borderRadius="md"
+                  transition={hoverTransition}
+                  _hover={{ transform: hoverScale, boxShadow: hoverShadow }}
+                  marginBottom={i === news.length - 1 ? "0" : "1.5rem"}
+                >
+                  <AccordionButton onClick={() => redirectToDetail(n.contentAsText)}>
+                    <Box flex="1" textAlign="left" fontSize="1.25rem" fontWeight="semibold" color={cardTextColor}>
+                      <Icon as={FiFileText} marginRight="12px" />
+                      {n.name} ｜ {n.date}
+                    </Box>
+                  </AccordionButton>
+                  <Text px={12} pb={4} color="gray.600">
+                    点击上方,开始阅读{n.name}的相关文章。
+                  </Text>
+                </AccordionItem>
+              ))}
+            </Accordion>
+          ) : (
+            <CircularProgress isIndeterminate color="blue.600" />
+          )}
+          <Paginator index={index} setIndex={setIndex} pages={pages} />
+        </Box>
       </Box>
     </Box>
   );
